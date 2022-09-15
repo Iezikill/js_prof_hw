7474be0d355a2e3860e35145acc2cd0becf43ff0
Viktoriya <viktoriya.shiryaeva54@yandex.ru>
2022-09-15 23:41:54 +0200

    html styling

--- 1/project/js/main.js
+++ 1/project/js/main.js
@@ -1,20 +1,21 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    { id: 1, title: 'Notebook', price: 2000 },
+    { id: 2, title: 'Mouse', price: 20 },
+    { id: 3, title: 'Keyboard', price: 200 },
+    { id: 4, title: 'Gamepad', price: 50 },
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
 const renderProduct = (title, price) => {
     return `<div class="product-item">
+                <img src="img/test_pic.jpg" alt="test_picture">
                 <h3>${title}</h3>
                 <p>${price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title,item.price));
+    const productsList = list.map(item => renderProduct(item.title, item.price)).join('');
     console.log(productsList);
     document.querySelector('.products').innerHTML = productsList;
 };
