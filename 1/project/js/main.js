c3484a09d464d23f7f25f3fa5949a5482b7debdd
Viktoriya <viktoriya.shiryaeva54@yandex.ru>
2022-09-15 23:56:22 +0200

    renderProduct's optimization

--- 1/project/js/main.js
+++ 1/project/js/main.js
@@ -4,20 +4,26 @@ const products = [
     { id: 3, title: 'Keyboard', price: 200 },
     { id: 4, title: 'Gamepad', price: 50 },
 ];
-//Функция для формирования верстки каждого товара
-//Добавить в выводе изображение
-const renderProduct = (title, price) => {
+
+/**
+ * Функция для формирования верстки каждого товара
+ * @param {*} product объект, содержащий id, наименование и цену товара
+ * @returns возвращает html код формирующий каждый товар
+ */
+const renderProduct = (product) => {
     return `<div class="product-item">
                 <img src="img/test_pic.jpg" alt="test_picture">
-                <h3>${title}</h3>
-                <p>${price}</p>
+                <h3>${product.title}</h3>
+                <p>${product.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
+
+const productsEl = document.querySelector('.products');
+
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price)).join('');
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    const productsList = list.map(item => renderProduct(item)).join('');
+    productsEl.innerHTML = productsList;
 };
 
 renderPage(products);
