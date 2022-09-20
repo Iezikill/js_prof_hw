0382c2ca3a1c17e70664105d6b35a9a91f065155
Viktoriya <viktoriya.shiryaeva54@yandex.ru>
2022-09-20 20:01:41 +0200

    layout of burger cafes

---
+++ Burgers/js/main.js
@@ -0,0 +1,60 @@
+'use strict'
+
+class Hamburger {
+  constructor(size, stuffing) {
+    this.size = size;
+    this.stuffing = stuffing;
+  }
+
+  /**
+   * Добавить добавку
+   * @param {*} topping 
+   */
+  addTopping(topping) {
+
+  }
+
+  /**
+   * Убрать добавку
+   * @param {*} topping 
+   */
+  removeTopping(topping) {
+
+  }
+
+  /**
+   * Получить список добавок
+   * @param {*} topping 
+   */
+  getToppings(topping) {
+
+  }
+
+  /**
+   * Узнать размер гамбургера
+   */
+  getSize() {
+
+  }
+
+  /**
+  * Узнать начинку гамбургера
+  */
+  getStuffing() {
+
+  }
+
+  /**
+  * Узнать цену
+  */
+  calculatePrice() {
+
+  }
+
+  /**
+  * Узнать калорийность
+  */
+  calculateCalories() {
+
+  }
+}
