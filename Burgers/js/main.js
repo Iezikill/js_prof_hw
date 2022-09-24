247fb9530b3a518cc09f1cee19bd383de2942efa
Viktoriya <viktoriya.shiryaeva54@yandex.ru>
2022-09-24 13:35:51 +0200

    burger in progress

--- Burgers/js/main.js
+++ Burgers/js/main.js
@@ -1,60 +1,84 @@
 'use strict'
+const priceEl = document.querySelector('.price span');
+const caloriesEl = document.querySelector('.calories span');
+document.querySelector('.order_button').addEventListener('load', event => {
+  event.preventDefault();
+  let burger = new Burger('size', 'stuffing', 'toppings');
+  // console.log(burger);
+  burger.showSum("priceEl", "caloriesEl");
+});
 
-class Hamburger {
-  constructor(size, stuffing) {
-    this.size = size;
-    this.stuffing = stuffing;
+class Param {
+  constructor(element) {
+    this.name = element.value;
+    this.price = +element.dataset.price;
+    this.calories = +element.dataset.calories;
   }
+}
 
-  /**
-   * Добавить добавку
-   * @param {*} topping 
-   */
-  addTopping(topping) {
-
-  }
-
-  /**
-   * Убрать добавку
-   * @param {*} topping 
-   */
-  removeTopping(topping) {
-
+class Burger {
+  constructor(size, stuffing, topping) {
+    this.size = new Param(this._select(size));
+    this.stuffing = new Param(this._select(stuffing));
+    this.toppings = this._getToppings(topping);
   }
 
   /**
    * Получить список добавок
-   * @param {*} topping 
+   * @param {*} name название дополнительных ингридиентов 
    */
-  getToppings(topping) {
-
+  getToppings(name) {
+    let result = [];
+    this._selectAll(name).forEach(el => {
+      let obj = new Param(el);
+      result.push(obj);
+    });
+    return result;
   }
 
   /**
-   * Узнать размер гамбургера
+   * Определяет выбранный элемент инпута
+   * @param {*} name 
+   * @returns 
    */
-  getSize() {
-
+  _select(name) {
+    return document.querySelector(`input[name=${name}]:checked`);
   }
 
   /**
-  * Узнать начинку гамбургера
-  */
-  getStuffing() {
-
+   * Определяет, где стоят галочки у чекбокса
+   * @param {*} name 
+   * @returns 
+   */
+  _selectAll(name) {
+    return [...document.querySelectorAll(`input[name=${name}]:checked`)];
   }
 
   /**
   * Узнать цену
   */
-  calculatePrice() {
-
+  _calculatePrice() {
+    let result = this.size.price + this.stuffing.price;
+    this.toppings.forEach(el => result += el.price);
+    return result;
   }
 
   /**
   * Узнать калорийность
   */
-  calculateCalories() {
+  _calculateCalories() {
+    let result = this.size.calories + this.stuffing.calories;
+    this.toppings.forEach(el => result += el.calories);
+    return result;
+  }
 
+  /**
+   * 
+   * @param {*} price 
+   * @param {*} calories 
+   */
+  showSum(price, calories) {
+    document.querySelector('.price').textContent = this._calculatePrice();
+    document.querySelector('.calories').textContent = this._calculateCalories();
   }
 }
