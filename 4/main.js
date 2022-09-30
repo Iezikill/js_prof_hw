62062546392c3171869ef25119f5f5f191315d10
Viktoriya <viktoriya.shiryaeva54@yandex.ru>
2022-09-30 21:15:10 +0200

    validator in progress

--- 4/main.js
+++
@@ -1,6 +0,0 @@
-'Use strict'
-
-const textEl = document.querySelector('.text');
-document.querySelector('button').addEventListener('click', () => {
-  textEl.textContent = textEl.textContent.replace(/\B'|'\B/g, '"');
-});
